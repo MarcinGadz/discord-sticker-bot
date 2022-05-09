a6935b70759be1e2bd352312960894be710dbadf
marcin <marcingadz@gmail.com>
2022-05-09 23:01:29 +0200

    Adds lombok

--- src/main/java/com/zzpj/dc/app/model/Image.java
+++ src/main/java/com/zzpj/dc/app/model/Image.java
@@ -1,76 +1,16 @@
 package com.zzpj.dc.app.model;
 
-import java.util.Arrays;
-import java.util.Objects;
+import lombok.*;
 
-public final class Image {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Image {
     private String name;
     private Byte[] content;
+    @ToString.Exclude
     private String owner;
     private Long saveDate;
-
-    public Image() {
-    }
-
-    public Image(String name, Byte[] content, String owner, Long saveDate) {
-        this.name = name;
-        this.content = content;
-        this.owner = owner;
-        this.saveDate = saveDate;
-    }
-
-    public Long getSaveDate() {
-        return saveDate;
-    }
-
-    public void setSaveDate(Long saveDate) {
-        this.saveDate = saveDate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Byte[] getContent() {
-        return content;
-    }
-
-    public void setContent(Byte[] content) {
-        this.content = content;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (Image) obj;
-        return Objects.equals(this.name, that.name) &&
-                Arrays.equals(this.content, that.content) &&
-                Objects.equals(this.owner, that.owner);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, owner);
-    }
-
-    @Override
-    public String toString() {
-        return "Image[" +
-                "name=" + name + ", " +
-                "owner=" + owner + ']';
-    }
-
 }
--- src/main/java/com/zzpj/dc/app/model/Owner.java
+++ src/main/java/com/zzpj/dc/app/model/Owner.java
@@ -1,34 +1,17 @@
 package com.zzpj.dc.app.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.LinkedList;
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class Owner {
     private String id;
     private List<Image> lastUsed = new LinkedList<>();
     private List<Image> ownImages = new LinkedList<>();
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public List<Image> getLastUsed() {
-        return lastUsed;
-    }
-
-    public void setLastUsed(List<Image> lastUsed) {
-        this.lastUsed = lastUsed;
-    }
-
-    public List<Image> getOwnImages() {
-        return ownImages;
-    }
-
-    public void setOwnImages(List<Image> ownImages) {
-        this.ownImages = ownImages;
-    }
 }
